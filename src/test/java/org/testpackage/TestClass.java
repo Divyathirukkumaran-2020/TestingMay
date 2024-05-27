e00a8c5359bcfecc53e9870589a456e8dd4597be
divyaganeshkumar <divyaganeshkumaran20@gmail.com>
2024-05-27 19:50:02 +0530

    Commiteed

---
+++ src/test/java/org/testpackage/TestClass.java
@@ -0,0 +1,34 @@
+package org.testpackage;
+
+public class TestClass {
+	public static void main(String[] args) {
+		int arr[] = {2,4,5,8,9,10};
+		int target = 7;
+		
+		for (int i = 0; i < arr.length; i++) {			
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i]+arr[j] == target) {
+					System.out.println(i);
+					System.out.println(j);
+				}
+				
+				
+			}
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
